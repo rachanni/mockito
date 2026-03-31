71b85c2063150e322dc48bccd1d759c2664c3568
raviprabhat.tcs <developer1-tc@uidai.net.in>
2026-03-31 18:37:59 +0530

    4. Creating Mock Test double using Mockito

--- src/test/java/com/hubberspot/mockito/junit/mock/MockTest.java
+++ src/test/java/com/hubberspot/mockito/junit/mock/MockTest.java
@@ -3,6 +3,7 @@ package com.hubberspot.mockito.junit.mock;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
 
@@ -25,9 +26,32 @@ public class MockTest {
 
         bookRepositoryMock.verify(book2, 1);
 
+    }
+
+    @Test
+    public void mockTestDoubleWithMockito(){
+
+//      Mocking BookRepository
+        BookRepository bookRepository = Mockito.mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        Book book1 = new Book("1234", "Mockito In Action", 500, LocalDate.now());
+        Book book2 = new Book("5127", "Junit5 In Action", 400, LocalDate.now());
 
 
+        bookService.addBook(book1); // return
+        bookService.addBook(book2); // save will be called
 
+//      for book2 save() will be called -> test pass
+        Mockito.verify(bookRepository).save(book2);
+
+//        for book1 save() will not be called
+//        Mockito.verify(bookRepository).save(book1);
+
+//      for book2 save() will be called 1 times
+        Mockito.verify(bookRepository, Mockito.times(1)).save(book2);
+//      for book1 save() will be called 0 times
+        Mockito.verify(bookRepository, Mockito.times(0)).save(book1);
 
     }
 }
